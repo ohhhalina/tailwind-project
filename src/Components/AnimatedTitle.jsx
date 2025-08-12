b409b443a54f0c1470b4ae293f42b93fc2b74b17
Alina Akchurina <ohhalina13@gmail.com>
2025-08-13 01:00:04 +0300

    animatedtitle

---
+++ src/Components/AnimatedTitle.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useRef } from 'react'
+import gsap from 'gsap';
+
+const AnimatedTitle = ({title, containerClass}) => {
+    const containerRef = useRef(null);
+
+    useEffect(() => {
+        const ctx = gsap.context(() => {
+            const titleAnimation = gsap.timeline({
+                scrollTrigger: {
+                    trigger: containerRef.current,
+                    start: '100 bottom',
+                    end: 'center center',
+                    toggleActions: 'play none none reverse'
+                }
+            })
+        }, containerRef)
+    }, []);
+
+  return (
+    <div
+    ref={containerRef}
+    className={`animated-title ${containerClass}`}>
+        {title.split('<br />').map((line, index) => (
+            <div key={index} className='flex-center max-w-full
+            flex-wrap gap-2 px-10 md:gap-3'>
+            {line.split(' ').map((word, i) => (
+                <span key={i} className='animated-word'
+                dangerouslySetInnerHTML={{ __html: word}} />
+            ))}
+            </div>
+        ))}
+    </div>
+  )
+}
+
+export default AnimatedTitle
