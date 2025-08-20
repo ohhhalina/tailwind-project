9f5e095ac35c0eed41bd6652b25d72dd533ffdaa
Alina Akchurina <ohhalina13@gmail.com>
2025-08-20 23:15:19 +0300

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react'
 import { TiLocationArrow } from 'react-icons/ti';
 import Button from './Button';
 import { useWindowScroll } from 'react-use';
+import gsap from 'gsap';
 
 
 const navItems = ['Nexus', 'Vault', 'Prologue', 'About', 'Contact']
@@ -20,8 +21,30 @@ const Navbar = () => {
     const {y: currentScrollY} = useWindowScroll();
 
     useEffect(() => {
+        if(currentScrollY === 0) {
+            setIsNavVisible(true);
+            navContainerRef.current.classList.remove
+        ('floating-nav')
+        } else if (currentScrollY > lastScrollY) {
+            setIsNavVisible(false);
+            navContainerRef.current.classList.add
+        ('floating-nav')
+        } else if (currentScrollY < lastScrollY) {
+            setIsNavVisible(true);
+            navContainerRef.current.classList.add
+        ('floating-nav')
+        }
 
-    }, [currentScrollY])
+        setLastScrollY(currentScrollY);
+    }, [currentScrollY, lastScrollY])
+
+    useEffect(() => {
+        gsap.to(navContainerRef.current, {
+            y: isNavVisible ? 0 : -100,
+            opacity: isNavVisible ? 1 : 0,
+            duration: 0.2,
+        })
+    }, [isNavVisible])
 
     const toggleAudioIndicator = () => {
         setIsAudioPlaying((prev) => !prev)
