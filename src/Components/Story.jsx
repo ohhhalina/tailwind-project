7c77dca32d67180f5ba52de5d30b6c5460ab6082
Alina Akchurina <ohhalina13@gmail.com>
2025-08-21 20:03:42 +0300

    story

--- src/Components/Story.jsx
+++ src/Components/Story.jsx
@@ -1,7 +1,48 @@
 import React from 'react'
 import AnimatedTitle from './AnimatedTitle'
+import { useRef } from 'react'
+import gsap from 'gsap'
 
 const Story = () => {
+
+const frameRef = useRef('null')
+
+const handleMouseLeave = () => {
+const element = frameRef.current;
+
+gsap.to(element, {
+        duration: 0.3,
+        rotateX: 0,
+        rotateY: 0, 
+        ease: 'power1.inOut'
+    })
+}
+
+const handleMouseMove = (e) => {
+    const { clientX, clientY } = e;
+    const element = frameRef.current
+
+    if(!element) return;
+
+    const rect = element.getBoundingClientRect()
+    const x = clientX - rect.left
+    const y = clientY - rect.top
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = ((y - centerY) / centerY) * -10;
+    const rotateY = ((x - centerX) / centerX) * 10;
+
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX,
+      rotateY,
+      transformPerspective: 500,
+      ease: "power1.inOut",
+    });
+}
+
   return (
     <section id='story' className='min-h-dvh w-screen bg-black text-blue-50'>
         <div className='flex size-full flex-col items-center
@@ -18,7 +59,22 @@ const Story = () => {
             />
             </div>
 
-            
+            <div className='story-img-container'>
+            <div className='story-img-mask'>
+            <div className='story-img-content'>
+                <img
+                ref={frameRef}
+                onMouseLeave={handleMouseLeave}
+                onMouseUp={handleMouseLeave}
+                onMouseEnter={handleMouseLeave}
+                onMouseMove={handleMouseMove}
+                src="img/entrance.webp"
+                alt="entrance.webp"
+                className='object-contain'
+                />
+            </div>
+            </div>
+            </div>
         </div>
     </section>
   )
